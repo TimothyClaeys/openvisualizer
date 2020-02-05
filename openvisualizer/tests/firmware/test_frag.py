7549166901f5cdca95a1d87187d5083000d41bd8
Timothy Claeys <timothy.claeys@gmail.com>
2020-02-05 19:26:27 +0100

    add corner case test for fragmentation

--- openvisualizer/tests/firmware/test_frag.py
+++ openvisualizer/tests/firmware/test_frag.py
@@ -1,3 +1,4 @@
+import time
 from random import randint
 
 import pytest
@@ -27,7 +28,7 @@ def pytest_generate_tests(metafunc):
 
 # ============================ tests ===========================================
 
-@pytest.mark.parametrize("payload_len", range(100, 1200, 100))
+@pytest.mark.parametrize("payload_len", range(100, 1000, 100))
 def test_basic_6lo_fragmentation(etun, ov_env, my_addr, node_addr, payload_len):
     """
     Test basic 6LoWPAN fragmentation and reassembly functions
@@ -61,6 +62,46 @@ def test_basic_6lo_fragmentation(etun, ov_env, my_addr, node_addr, payload_len):
     if timeout:
         # node to failed to respond with an ICMPv6 echo before timeout
         pytest.fail("Timeout on ICMPv6 Echo Response!")
+
+    # let the network recover between the different tests
+
+
+@pytest.mark.parametrize("payload_len", range(30, 100, 1))
+def test_corner_cases_6lo_frag(etun, ov_env, my_addr, node_addr, payload_len):
+    """
+    Test the corner case of the fragmentation code by changing the payload size byte by byte
+    """
+    _, node_joined = ov_env
+    if node_addr not in node_joined:
+        pytest.skip("Node with address {} never joined, skipping this test".format(node_addr))
+
+    ip = IPv6(src=my_addr, dst=node_addr, hlim=64)
+    id = randint(0, 65535)
+    seq = randint(0, 65535)
+    icmp = ICMPv6EchoRequest(id=id, seq=seq)
+    pkt = ip / icmp
+
+    payload = "".join([chr(randint(0, 255)) for b in range(payload_len)])
+    pkt.add_payload(payload)
+
+    etun.write(list(bytearray(raw(pkt))))
+    received = etun.read(dest=my_addr, timeout=25)
+
+    timeout = True
+    for recv_pkt in received:
+        ipv6_pkt = IPv6(recv_pkt)
+        if is_my_icmpv6(ipv6_pkt, node_addr, my_addr):
+            timeout = False
+            icmp = ICMPv6EchoReply(raw(ipv6_pkt)[40:])
+            # check if icmp headers match
+            assert icmp.id == id
+            assert icmp.seq == seq
+
+    if timeout:
+        # node to failed to respond with an ICMPv6 echo before timeout
+        pytest.fail("Timeout on ICMPv6 Echo Response!")
+
+    # let the network recover between the different tests
 
 
 @pytest.mark.sim_only
